ae2e56bebf4df68155523dfbff12102d6f977317
Abbas Roholamin <abbasroholamin76@gmail.com>
2022-09-15 22:54:56 +0430

    Write user function

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -17,4 +17,14 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    /**
+     * Each chip is associated with a user
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
