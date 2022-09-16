54fea00f1ead020300da3b7d4798cf145a7fb7fe
Abbas Roholamin <abbasroholamin76@gmail.com>
2022-09-16 11:58:20 +0430

    Write dispatch chirp event

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Events\ChirpCreated;
 
 class Chirp extends Model
 {
@@ -16,6 +17,15 @@ class Chirp extends Model
      */
     protected $fillable = [
         'message',
+    ];
+
+    /**
+     * dispatchesEvents
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     /**
